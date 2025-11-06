82a32520de4107584eb53c393bd182c9ddb2c5aa
nielskloster <niels@zetland.dk>
2025-11-06 13:19:23 +0100

    Try to setup Mainframe mcp

--- lib/generate-response.ts
+++ lib/generate-response.ts
@@ -1,10 +1,10 @@
 import { openai } from "@ai-sdk/openai";
 import { CoreMessage, generateText, tool } from "ai";
-import { z } from "zod";
+import { tools } from "./mainframe-mcp.js";
 
 export const generateResponse = async (
   messages: CoreMessage[],
-  updateStatus?: (status: string) => void,
+  updateStatus?: (status: string) => void
 ) => {
   const { text } = await generateText({
     model: openai("gpt-4o"),
@@ -14,31 +14,7 @@ export const generateResponse = async (
     - Put sources inline if possible.`,
     messages,
     maxSteps: 10,
-    tools: {
-      getWeather: tool({
-        description: "Get the current weather at a location",
-        parameters: z.object({
-          latitude: z.number(),
-          longitude: z.number(),
-          city: z.string(),
-        }),
-        execute: async ({ latitude, longitude, city }) => {
-          updateStatus?.(`is getting weather for ${city}...`);
-
-          const response = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode,relativehumidity_2m&timezone=auto`,
-          );
-
-          const weatherData = await response.json();
-          return {
-            temperature: weatherData.current.temperature_2m,
-            weatherCode: weatherData.current.weathercode,
-            humidity: weatherData.current.relativehumidity_2m,
-            city,
-          };
-        },
-      }),
-    },
+    tools: tools,
   });
 
   // Convert markdown to Slack mrkdwn format
