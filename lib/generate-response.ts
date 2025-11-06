c75037a3b42aa805f72b74eef1efaeec6bf09ecb
nielskloster <niels@zetland.dk>
2025-11-06 13:26:44 +0100

    Try with one file

--- lib/generate-response.ts
+++ lib/generate-response.ts
@@ -1,6 +1,14 @@
 import { openai } from "@ai-sdk/openai";
 import { CoreMessage, generateText } from "ai";
-import { mainframe } from "./mainframe-mcp";
+import { experimental_createMCPClient as createMCPClient } from "@ai-sdk/mcp";
+
+const mainframe = createMCPClient({
+  transport: {
+    type: "http",
+    url: "https://api-staging.zetland.dk/api/v1/internal/mcp",
+    headers: { "X-Internal-Api-Key": process.env.MAINFRAME_API_KEY! },
+  },
+});
 
 export const generateResponse = async (
   messages: CoreMessage[],
--- lib/mainframe-mcp.ts
+++
@@ -1,9 +0,0 @@
-import { experimental_createMCPClient as createMCPClient } from "@ai-sdk/mcp";
-
-export const mainframe = createMCPClient({
-  transport: {
-    type: "http",
-    url: "https://api-staging.zetland.dk/api/v1/internal/mcp",
-    headers: { "X-Internal-Api-Key": process.env.MAINFRAME_API_KEY! },
-  },
-});
